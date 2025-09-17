c491a43c62d620a6c8c91a1ec24208653d08c783
Sparsh Tyagi <tyagisarthak53@gmail.com>
2025-09-17 20:15:59 +0530

    Created My 1st Comp

---
+++ Explore/src/MyComp.jsx
@@ -0,0 +1,19 @@
+function MyComp(){
+  return(
+    <div>
+      <h1>
+        hello My First Comp
+      </h1>
+      <fieldset>
+        <legend>
+          Im sparsh
+          
+        </legend>
+        <p>
+          Im a 20yr old boy .
+        </p>
+      </fieldset>
+    </div>
+  );
+}
+export default MyComp;
