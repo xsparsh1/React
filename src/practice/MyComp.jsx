efde9345cac9bfe8611bd5f999dbfd0a9a486c7c
Sparsh Tyagi <tyagisarthak53@gmail.com>
2025-09-18 19:41:12 +0530

    Fixed Indentation

---
+++ src/practice/MyComp.jsx
@@ -0,0 +1,20 @@
+import OnClick from "./onClick";
+
+
+function MyComp(){
+  return(
+    <div>
+
+      <h1>hello My First Comp</h1>
+
+      <fieldset>
+        <legend>Im sparsh</legend>
+        <p>Im a 20yr old boy .</p>
+      </fieldset>
+
+      <OnClick/>
+      
+    </div>
+  );
+}
+export default MyComp;
