c3ede122e66ff7cba42856ac0e4a712c0bb2820d
Sparsh Tyagi <tyagisarthak53@gmail.com>
2025-09-19 20:49:33 +0530

    Toggle based props

--- src/practice/MountFiles/LoadProps.jsx
+++ src/practice/MountFiles/LoadProps.jsx
@@ -1,10 +1,19 @@
 import Prop1 from "../Props/Prop1";
+import Prop2 from "../Props/Prop2";
+import {useState} from "react";
+
 function LoadProps(){
     const name="Hello";
     const age=25;
+    const [toggle,setToggle]=useState(false);
+    const handleChange=()=>{
+        setToggle(!toggle)
+    }
     return(
         <div>
             <Prop1 value={name} value2={age}/>
+            {toggle && <Prop2 value="okay"/>}
+            <button onClick={handleChange}>Click</button>
         </div>
     )
 }
---
+++ src/practice/Props/Prop2.jsx
@@ -0,0 +1,8 @@
+function Prop2(prop){
+    return(
+        <div>
+            <h1>You can toggle this prop {prop.value}</h1>
+        </div>
+    )
+}
+export default Prop2;
