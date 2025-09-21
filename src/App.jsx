7d87b77137833616456a3fa7bb7d17d939ada46f
Sparsh Tyagi <tyagisarthak53@gmail.com>
2025-09-21 15:31:51 +0530

    Using For loop in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import GetInput from "./practice/InputFeild/GetInput";
 import Radio_DropDown from "./practice/InputFeild/Radio_DropDown";
 import LoadBaseInfo from "./practice/MountFilesProps/LoadBaseInfo";
 import LoadProps from "./practice/MountFilesProps/LoadProps";
+import ForLoop from "./practice/ForLoopInJsx/forLoop";
 
 function App(){
   return (
@@ -13,7 +14,8 @@ function App(){
         {/* <GetInput/> */}
         {/* <CC/> */}
         {/* <Checkbox/> */}
-        <Radio_DropDown/>
+        {/* <Radio_DropDown/> */}
+        <ForLoop/>
   </div>
   );
 }
---
+++ src/practice/ForLoopInJsx/forLoop.jsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+
+
+function ForLoop(){
+
+    const arr=[
+        {
+            name:"Sparsh",
+            age:19,
+            id:1
+        },
+        {
+            name:"Harsh",
+            age:18,
+            id:12
+        },
+        {
+            name:"Vansh",
+            age:17,
+            id:13
+        }
+    ];
+
+    return(
+        <div className="div">
+            {arr.map((value)=>
+            <div key={value.id}>             {/*React used key to uniquely identify each element in list. */}
+                <h3>{value.name}</h3>
+                <p>Age is {value.age}</p>
+            </div>
+            )}
+        </div>
+    )
+}
+
+export default ForLoop;
