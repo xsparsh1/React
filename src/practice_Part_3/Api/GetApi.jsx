d880cfa6c26fe10feaaaa7231ccbc8293c5a6c55
Sparsh Tyagi <tyagisarthak53@gmail.com>
2025-10-03 13:42:04 +0530

    Get Api in React

---
+++ src/practice_Part_3/Api/GetApi.jsx
@@ -0,0 +1,33 @@
+import {useEffect, useState} from 'react';
+
+export function GetApi(){
+
+    const [details,setDetails]=useState([]);
+
+    useEffect(()=>{
+        makeCall();
+    },[])
+
+    async function makeCall(){
+        const url="https://dummyjson.com/users";
+        let response=await fetch(url);                  //fetch() returns promise therefore used await 
+        response=await response.json();                 //Sae with json()
+        setDetails(response.users);
+    }    
+
+    return(
+        <>
+        <h1 style={{margin:"20px 0"}}>Geting Data from Api: </h1>
+            <ul style={{marginLeft:"30px"}}>
+                {
+                    details && details.map((item)=>(
+                
+                    <li key={item.id}>
+                        {item.age} , {item.firstName} , {item.lastName}
+                    </li>
+                    ))
+                }
+                </ul>
+        </>
+    )
+}
